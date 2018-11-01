173586c7435bbe6f6381b0cde86c0f0668651d2f
fickrybiliman <bil.iman.fickry@gmail.com>
2018-11-02 00:54:02 +0700

    kali terus rekursif done, Alhamdulillah, lumayan pusing tapi nya..

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1.  Exercise Faktorial Rekursif
 2.  Exercise Total Digit Rekursif
+3.  Exercise Kali Terus Rekursif
---
+++ kaliTerusRekursif.js
@@ -0,0 +1,18 @@
+function kaliTerusRekursif(angka) {
+  // you can only write your code here!
+  angka = angka.toString().split('');
+  let result = angka.reduce((accumulator, currentValue) => accumulator * currentValue);
+
+  if (result < 10) {
+    return result;
+  } else {
+    return kaliTerusRekursif(result);
+  }
+}
+
+// TEST CASES
+console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(3)); // 3
+console.log(kaliTerusRekursif(24)); // 8
+console.log(kaliTerusRekursif(654)); // 0
+console.log(kaliTerusRekursif(1231)); // 6
