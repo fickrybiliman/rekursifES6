eab96684422d8a14cd7fd7fd10e71e7dc2b85879
fickrybiliman <bil.iman.fickry@gmail.com>
2018-11-09 16:33:32 +0700

    melengkapi setter dan getter, ternyata ketinggalan setter dan getter nya, hehehe, Alhamdulillah Done

--- objectStudent.js
+++ objectStudent.js
@@ -12,9 +12,42 @@ var studentObj = {
          sports: ['Badminton', 'Jogging', 'Swimming']
       }
    ],
-   getData: function() {
-      return  `Nama : ${this.name} \nUmur : ${this.age} \nTanggal Lahir : ${this.dateOfBirth} \nJenis kelamin : ${this.gender} \nId : ${this.studentID} \nHobby : ${this.hobbies} \nSport : ${this.hobbies[this.hobbies.length-1].sports}`;
+   setName: function(newName) {
+      this.name = newName;
+   },
+   setAge: function(newAge) {
+      this.age = newAge;
+   },
+   setDateOfBirth: function(newDateOfBirth) {
+      this.dateOfBirth = newDateOfBirth;
+   },
+   setGender: function(newGender) {
+      if (newGender === 'Male' || newGender === 'Female') {
+         this.gender = newGender;
+      } else {
+         console.log('Gender yang dimasukkan salah');
+      }
+   },
+   addHobby: function(newHobby) {
+      this.hobbies.push(newHobby);
+   },
+   removeHobby: function(hobby) {
+      let hobbyIndex = this.hobbies.indexOf(hobby);
+      this.hobbies.splice(hobbyIndex, 1);
+   },
+   getData: function() {    
+      let sportIndex;
+      for (let i = 0; i < this.hobbies.length; i++) {
+         if (typeof this.hobbies[i] === 'object') {
+            sportIndex = i;
+         }
+      }
+      return  `Nama : ${this.name} \nUmur : ${this.age} Tahun \nTanggal Lahir : ${this.dateOfBirth} \nJenis kelamin : ${this.gender} \nId : ${this.studentID} \nHobby : ${this.hobbies} \nSport : ${this.hobbies[sportIndex].sports}`;
    }
 };
 
+studentObj.setName('Fathin Kholidi Bil Iman');
+studentObj.setAge(8);
+studentObj.setDateOfBirth('08 Agustus 2010');
+studentObj.addHobby('Nonton Doraemon');
 console.log(studentObj.getData());
